c46634e5540d2ba332461300b9191735b76d65b0
meghakundu <54468245+meghakundu@users.noreply.github.com>
2023-10-05 11:30:48 +0530

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->integer('age');
+            $table->boolean('walking_status');
+            $table->foreign('enroute_id')
+            ->references('id')->on('enroutes')->onDelete('cascade');
+            $table->string('address');
+            $table->string('city');
+            $table->string('gender');
+            $table->string('walking_pace');
+            $table->string('past_illness');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
